886a440539c2f4bf15abd9fb45fb4f8b6ce6313b
Anna Luchechko <annaluchechko@gmail.com>
2020-12-01 17:11:16 +0200

    Create project

---
+++ SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID
+//
+//  Created by Anna Luchechko on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
